819df2cd036db773c0bb7d9eb4171055e345841f
Leonardo Henrique de Almeida Pacheco <leonardohenriquedealmeidapacheco@MacBook-Pro-de-Leonardo.local>
2023-07-20 20:43:21 -0300

    ajustando pastas

--- App/backend/config/rabbit.ts
+++ App/backend/config/rabbit.ts
@@ -3,13 +3,9 @@ import { RabbitConfig } from '@ioc:Adonis/Addons/Rabbit'
 
 const rabbitConfig: RabbitConfig = {
   hostname: Env.get('RABBITMQ_HOSTNAME', 'localhost') as string,
-
   user: Env.get('RABBITMQ_USER') as string,
-
   password: Env.get('RABBITMQ_PASSWORD') as string,
-
   port: Number(Env.get('RABBITMQ_PORT')),
-  
   protocol: Env.get('RABBITMQ_PROTOCOL'),
 }
 
